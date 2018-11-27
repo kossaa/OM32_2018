c181fc3d8482bbdacbd0a9da5a7cc2aff106923a
ru-ya <44492499+ru-ya@users.noreply.github.com>
2018-11-27 15:42:49 +0900

    予約画面
    とりあえず現状の最新版

--- OICHINEMA/WebApplication1/Reservation_Ticket_Selection.aspx.cs
+++ OICHINEMA/WebApplication1/Reservation_Ticket_Selection.aspx.cs
@@ -12,8 +12,6 @@ namespace WebApplication1
 {
     public partial class Reservation_Ticket_Selection : System.Web.UI.Page
     {
-        OleDbCommand cmd;
-        DataSet ds;
         int seatcount = 0;
         string[] seatname = new string[1];
         const int Cell2Width = 300;
@@ -21,6 +19,13 @@ namespace WebApplication1
         Button BackBtn = new Button();
         Button NextBtn = new Button();
         TextBox MailAddressTextbox = new TextBox();
+        string[] eventname = new string[1];//イベント名を格納
+        int[] eventrate = new int[1];//イベント料金を格納
+        string[] eventprice = new string[1];//変更する料金番号を格納
+        string[] eventaddname = new string[1];
+        int[] eventaddrate = new int[1];
+        string[] displayticketname = new string[1];//表示されるチケット名を格納
+        int[] displayticketrate = new int[1];//表示されるチケット料金を格納
 
         private DropDownList[] TicketDDL;
 
@@ -30,34 +35,85 @@ namespace WebApplication1
             Session["ScheduleID"] = "0000005";
             Session["ScheduleEnd"] = DateTime.Parse("2018/10/25 14:25:00");
 
-            
+
             DateTime time = (DateTime)Session["ScheduleEnd"];
             OleDbConnection cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=|DataDirectory|BookingDB.accdb;");
-            ////cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=|DataDirectory|\\stusrv\システム開発演習\OM-32\データベース\BookingDB.accdb;");
-            OleDbDataAdapter daEvent = new OleDbDataAdapter("SELECT EVENT_ID,EVENT_NAME,EVENT_RATE,SCHEDULE_ID,EVENT_STARTDAY,EVENT_ENDDAY,RATE_ID FROM TBL_EVENT WHERE SCHEDULE_ID='" + (string)Session["ScheduleID"] + "' OR EVENT_STARTDAY<=#" + (DateTime)Session["ScheduleEnd"] + "# AND EVENT_ENDDAY>=#" + (DateTime)Session["ScheduleEnd"] + "#", cn);
+            //イベントテーブル
+            //その上映時に開催しているイベントの一覧を取得する
+            //dtEvent.Rows[0][0]イベント番号、[0][1]イベント名、[0][2]イベント料金、[0][3]スケジュール番号、[0][4]料金番号
+            OleDbDataAdapter daEvent = new OleDbDataAdapter("SELECT EVENT_ID,EVENT_NAME,EVENT_RATE,SCHEDULE_ID,RATE_ID FROM TBL_EVENT WHERE EVENT_STARTDAY<=#" + (DateTime)Session["ScheduleEnd"] + "# AND EVENT_ENDDAY>=#" + (DateTime)Session["ScheduleEnd"] + "# ORDER BY RATE_ID", cn);
             DataTable dtEvent = new DataTable();
             daEvent.Fill(dtEvent);
-            OleDbDataAdapter daRate = new OleDbDataAdapter("SELECT * FROM TBL_RATE WHERE RATE_START<=#" + ((DateTime)Session["ScheduleEnd"]).TimeOfDay + "# AND RATE_END>=#" + ((DateTime)Session["ScheduleEnd"]).TimeOfDay + "#", cn);
-            DataTable dtRate = new DataTable();
-            daRate.Fill(dtRate);
+            int scheduleflag = 0;//スケジュール番号がnullでないイベントがあった時にフラグを立てる
+            for (int i = 0; i < dtEvent.Rows.Count; i++)//スケジュール番号チェック
+            {
+                //スケジュール番号が登録されている場合
+                if (dtEvent.Rows[i][3].ToString() != "")
+                {
+                    scheduleflag = 1;
+                    eventname[0] = dtEvent.Rows[i][1].ToString();
+                    eventrate[0] = int.Parse(dtEvent.Rows[i][2].ToString());
+                    break;
+                }
+            }
+            //イベント配列に該当する条件のイベントを格納する
+            if (scheduleflag == 0)//スケジュール番号が登録されていなければ
+            {
+                for (int i = 0, j = 0,k=0; i < dtEvent.Rows.Count; i++)//イベントテーブルに該当するイベントが登録されていたら
+                {
+                    if (dtEvent.Rows[i][4].ToString() == "ADD")//追加されるチケットを格納
+                    {
+                        Array.Resize(ref eventaddname, k + 1);
+                        Array.Resize(ref eventaddrate, k + 1);
+                        eventaddname[k] = dtEvent.Rows[i][1].ToString();
+                        eventaddrate[k] = int.Parse(dtEvent.Rows[i][2].ToString());
+                        k++;
+                    }
+                    else//変更されるチケットを格納
+                    {
+                        //配列のサイズをチケットの種類数に合わせて変化させ、イベント内容を格納する
+                        Array.Resize(ref eventname, j + 1);
+                        Array.Resize(ref eventrate, j + 1);
+                        Array.Resize(ref eventprice, j + 1);
+                        eventname[j] = dtEvent.Rows[i][1].ToString();
+                        eventrate[j] = int.Parse(dtEvent.Rows[i][2].ToString());
+                        eventprice[j] = dtEvent.Rows[i][4].ToString();
+                        j++;
+                    }
+                }
+                //料金テーブル
+                //上映終了時間が開始時間と終了時間の間に含まれる料金の一覧を取得する
+                //dtRate.Rows[0][0]料金番号、[0][1]料金名、[0][2]料金
+                OleDbDataAdapter daRate = new OleDbDataAdapter("SELECT RATE_ID,RATE_NAME,RATE_PRICE FROM TBL_RATE WHERE RATE_START<=#" + ((DateTime)Session["ScheduleEnd"]).TimeOfDay + "# AND RATE_END>=#" + ((DateTime)Session["ScheduleEnd"]).TimeOfDay + "# ORDER BY RATE_ID", cn);
+                DataTable dtRate = new DataTable();
+                daRate.Fill(dtRate);
+                int ticketcount = 0;
+                for (int i = 0; i < dtRate.Rows.Count; i++,ticketcount++)
+                {
+                    Array.Resize(ref displayticketname, i + 1);
+                    Array.Resize(ref displayticketrate, i + 1);
+                    for (int j = 0; j < eventprice.Length; j++)
+                    {
+
+                    }
+                }
+                TicketDDL[ticketcount].Items.Add("6ポイントで一回無料");
+            }
             //ClientScriptManager cs = Page.ClientScript;
             //Type csType = this.GetType();
             //cs.RegisterStartupScript(csType, "onload", "<script type=\"text/javascript\">alert('"+表示したい値+"');</script>");
-            if (dtRate.Rows.Count != 0)//イベントテーブルに該当するイベントが登録されていたら
-            {
-                
-            }
+
+
+              
 
 
 
-            //イベントテーブル
-            //選択されたスケジュール番号が登録されているかどうかをチェックし、なければ現在の日付がイベントの開始日と終了日の間に入っているかをチェック。
-            //料金番号がallの場合表示される全ての料金をイベント料金に置き換える
-            //料金番号がaddの場合イベント料金の置き換えは発生させずにチケット一覧に追加する
-
-            //料金テーブル
             //設定時間のうちイベントテーブルをチェックした結果変更されなかった料金番号を表示する
-            
+
+
+            //取得した料金テーブルの料金番号と料金名を格納する配列をそれぞれ用意し、格納する。
+            //取得したイベントテーブルの料金番号を取得して料金番号配列の料金番号と照らし合わせてその位置にある料金名を置き換える
+
 
             //画面結合時に前の画面からデータを継承する
             //ログイン中の場合会員情報を取得する
@@ -72,7 +128,7 @@ namespace WebApplication1
 
             TableRow tableRow;
             TableCell tableCell;
-                        
+
             tableRow = new TableRow();
             tableCell = new TableCell();
             tableCell.Text = "席番号";
@@ -92,8 +148,13 @@ namespace WebApplication1
                 tableCell = new TableCell();
                 TicketDDL[i] = new DropDownList();
                 TicketDDL[i].Items.Add("チケットの種類を選択してください");
-                for (int j = 0; j < dtRate.Rows.Count; j++)
-                    TicketDDL[i].Items.Add(dtRate.Rows[j][1].ToString());
+                if (scheduleflag == 0)//スケジュール番号が登録されていないからチケットの種類が一つに絞られない場合
+                {
+                    for (int j = 0; j<displayticketname.Length; j++)//表示チケット名配列の数だけ繰り返す
+                        TicketDDL[i].Items.Add(displayticketname[j]);
+                }
+                else//スケジュール番号が登録されているからチケットが一種類しかない場合
+                    TicketDDL[i].Items.Add(eventname[0]);
                 tableCell.Controls.Add(TicketDDL[i]);
                 tableRow.Cells.Add(tableCell);
                 Table1.Rows.Add(tableRow);

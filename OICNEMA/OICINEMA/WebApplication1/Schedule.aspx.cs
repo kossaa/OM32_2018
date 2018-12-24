99d797474204ffe65fbfd56fb9f24149283a846d
SakuraiRiku <44692492+SakuraiRiku@users.noreply.github.com>
2018-12-24 17:01:55 +0900

    2018_12_24

---
+++ OICNEMA/OICINEMA/WebApplication1/Schedule.aspx.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        List<string> seatList = new List<string>();//選択中の席リスト
+
+        //データベース接続
+        OleDbConnection cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=|DataDirectory|BookingDB.accdb;");
+
+        private System.Drawing.Color tableChange(double ritu)
+        {
+            //色変更
+            if (ritu >= 0 && ritu <= 40)
+            {
+                //緑色
+                return System.Drawing.Color.Green;
+            }
+            else if (ritu >= 41 && ritu <= 80)
+            {
+                //黄色
+                return System.Drawing.Color.Yellow;
+            }
+            else if (ritu >= 81 && ritu < 100)
+            {
+                //赤色
+                return System.Drawing.Color.Red;
+            }
+            else
+            {
+                //黒色
+                return System.Drawing.Color.Black;
+            }          
+        }
+        //ロードイベント
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Table ScreenTable = new Table();
+            //DBでデータ取得
+            //スケジュールID設定(仮)
+            Session["ScheduleID"] = "0000004";
+            //座席の予約済みを取得し昇順で表示
+            OleDbDataAdapter daSeat = new OleDbDataAdapter
+            ("SELECT Count(BOOKINGDETAIL_ID), SEAT_AREA FROM TBL_BOOKING,TBL_BOOKINGDETAIL,TBL_SEAT WHERE TBL_BOOKING.BOOKING_ID = TBL_BOOKINGDETAIL.BOOKING_ID AND TBL_BOOKINGDETAIL.SEAT_ID = TBL_SEAT.SEAT_ID GROUP BY SCHEDULE_ID,SEAT_AREA HAVING (SCHEDULE_ID ='0000004') ORDER BY SEAT_AREA", cn);
+            //DataTableを作成し実行
+            DataTable dtSeat = new DataTable();
+            daSeat.Fill(dtSeat);
+            //仮登録版
+            OleDbDataAdapter daInfo = new OleDbDataAdapter
+            ("SELECT TEMPORARY_SEAT FROM TBL_TEMPORARY WHERE TEMPORARY_SCHEDULE='" + (string)Session["ScheduleID"] + "'", cn);
+            //DataTableを作成し実行
+            DataTable dtInfo = new DataTable();
+            daInfo.Fill(dtInfo);
+            //本登録版
+            //dt.Rows[0][0]スケジュール番号、[0][1]開始時間、[0][2]終了時間、[0][3]スクリーン番号、[0][4]作品名、[0][5]作品画像パス
+            OleDbDataAdapter daEntry = new OleDbDataAdapter
+            ("SELECT SCHEDULE_ID,SCHEDULE_START,SCHEDULE_END,SCREEN_ID,WORK_NAME,WORK_PASS1 FROM TBL_WORK,TBL_SCHEDULE WHERE TBL_WORK.WORK_ID = TBL_SCHEDULE.WORK_ID AND SCHEDULE_START>=#12/26/2018# AND SCHEDULE_START<#12/27/2018#", cn);
+            //DataTableを作成し実行
+            DataTable dtEntry = new DataTable();
+            daEntry.Fill(dtEntry);
+            //TableRowとTableCell            
+            TableRow tr;
+            TableCell tc;
+            Table tb;
+            //スケジュール作成
+            //3=映画のタイトル数
+            for (int i = 0; i < 3; i++)
+            {
+                //1行目作成
+                tr = new TableRow();
+                //4＝タイトル(1)＋スクリーン名（Count)3
+                for (int j = 0; j < dtEntry.Rows.Count; j++)
+                {
+                    tc = new TableCell();
+                    tc.BorderStyle = BorderStyle.Solid;
+                    tc.BorderColor = System.Drawing.Color.Red;
+                    if (j == 0)
+                    {
+                        tc.Text = dtEntry.Rows[j][4].ToString();
+                        tr.Cells.Add(tc);
+                    }
+                    else
+                    {
+                        tc.Text = dtEntry.Rows[j][3].ToString();
+                        tr.Cells.Add(tc);
+                    }
+                    Table1.Rows.Add(tr);
+                }
+                //2行目作成
+                tr = new TableRow();
+                //4＝画像(1)+スクリーンのエリア番号(9分割)３
+                for (int j = 0; j < 4; j++)
+                {
+                    //変数aはエリアIDのカウント
+                    int a = 0;
+                    tc = new TableCell();
+                    //中にセルを作成
+                    if (j == 0)
+                    {
+                        tc.BorderColor = System.Drawing.Color.Red;
+                        tc.Text = "画像";
+                        tc.RowSpan = 2;
+                        tr.Cells.Add(tc);
+                    }
+                    else
+                    {
+                        //セルの中にテーブル２作成
+                        tb = new Table();
+                        OleDbDataAdapter daArea = new OleDbDataAdapter
+                        ("SELECT Count(SEAT_ID) FROM TBL_SEAT GROUP BY SEAT_AREA", cn);
+                        //DataTableを作成し実行
+                        DataTable dtArea = new DataTable();
+                        daArea.Fill(dtArea);
+
+                        //テーブル２の中にセル作成
+                        for (int k = 0; k < 3; k++)
+                        {
+                            //テーブル２の行作成
+                            TableRow tr2 = new TableRow();
+                            for (int l = 0; l < 3; l++)
+                            {
+                                //２のセル作成                               
+                                TableCell tc2 = new TableCell();
+                                tc2.BackColor = tableChange((double)int.Parse(dtSeat.Rows[a][0].ToString()) / int.Parse(dtArea.Rows[a][0].ToString()) * 100);
+                                a++;
+                                tc2.Text = "エリア" + a.ToString();
+                                tr2.Controls.Add(tc2);                            
+                            }
+                            //テーブル追加前にテーブル２経由
+                            tb.Controls.Add(tr2);
+                        }
+                        tc.Controls.Add(tb);
+                        tr.Controls.Add(tc);
+                    }
+                    //テーブルに追加
+                    Table1.Rows.Add(tr);
+                }
+                //3行目作成
+                tr = new TableRow();
+                //for 中身
+                for (int j = 0; j < 3; j++)
+                {
+                    tc = new TableCell();
+                    tc.Text = "開始時間～終了時間";
+                    tr.Cells.Add(tc);
+                    Table1.Rows.Add(tr);
+                }
+            }
+        }
+        
+    }
+}
---
+++ OICNEMA/OICINEMA_チケット/WebApplication1/Reservation_Confirm_Input_Information.aspx.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class test : System.Web.UI.Page
+    {
+        const int Cell2Width = 300;
+        List<string> seatList = new List<string>();//座席選択画面から引き継いだ座席情報を格納
+        List<SelectedTicket> selectedticketList = new List<SelectedTicket>();//チケット選択画面から引き継いだ選択されたチケット情報を格納
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if ((string)Session["PageID"] != "Reservation_Ticket_Selection" && !IsPostBack)//正規の手順でこのフォームを表示しなかった場合
+            {
+                //各Sessionのクリア
+                Session.Remove("ScheduleID");
+                Session.Remove("ScheduleEnd");
+                Session.Remove("BookingMail");
+                Session.Remove("SeatInformation");
+                Session.Remove("SelectedTicket");
+                Session.Remove("WorkName");
+                Session.Remove("ScheduleStart");
+                Response.Redirect("Reservation_Ticket_Selection.aspx");
+                //Response.Redirect("TOP.aspx");//TOP画面に飛ぶ
+                return;
+            }
+            Session["PageID"] = "Reservation_Confirm_Input_Information";
+            seatList = (List<string>)Session["SeatInformation"];//Sessionに格納されてる選択された座席情報を使えるようにseatListに展開する
+            selectedticketList = (List<SelectedTicket>)Session["SelectedTicket"];//引き継いだ選択されたチケット情報を格納する
+
+            TableRow tableRow;
+            TableCell tableCell;
+
+            tableRow = new TableRow();
+            tableCell = new TableCell();
+            tableCell.Text = "上映タイトル：";
+            tableRow.Cells.Add(tableCell);
+            tableCell = new TableCell();
+            tableCell.Text = (string)Session["WorkName"];
+            tableCell.Width = Cell2Width;
+            tableRow.Cells.Add(tableCell);
+            Table1.Rows.Add(tableRow);
+
+            tableRow = new TableRow();
+            tableCell = new TableCell();
+            tableCell.Text = "氏名：";
+            tableRow.Cells.Add(tableCell);
+            tableCell = new TableCell();
+            if ((string)Session["MemberID"] != "")//ログインされていたら会員番号から会員名を取り出して表示する
+            {
+                OleDbConnection cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=|DataDirectory|BookingDB.accdb;");
+                OleDbDataAdapter daRate = new OleDbDataAdapter("SELECT MEMBER_NAME FROM TBL_MEMBER WHERE MEMBER_ID='" + (string)Session["MemberID"] + "'", cn);
+                DataTable dtRate = new DataTable();
+                daRate.Fill(dtRate);
+                tableCell.Text = dtRate.Rows[0][0].ToString();
+            }
+            else
+                tableCell.Text = "ログインされていません";
+            tableCell.Width = Cell2Width;
+            tableRow.Cells.Add(tableCell);
+            Table1.Rows.Add(tableRow);
+
+            tableRow = new TableRow();
+            tableCell = new TableCell();
+            tableCell.Text = "上映予定日時：";
+            tableRow.Cells.Add(tableCell);
+            tableCell = new TableCell();
+            tableCell.Text = ((DateTime)Session["ScheduleStart"]).ToString("yyyy年M月d日 H時m分") + "～" + ((DateTime)Session["ScheduleEnd"]).ToString("yyyy年M月d日 H時m分");
+            tableCell.ColumnSpan = 2;
+            tableCell.Width = Cell2Width;
+            tableRow.Cells.Add(tableCell);
+            Table1.Rows.Add(tableRow);
+
+            tableRow = new TableRow();
+            tableCell = new TableCell();
+            tableCell.Text = "メールアドレス：";
+            tableRow.Cells.Add(tableCell);
+            tableCell = new TableCell();
+            tableCell.Text = Session["BookingMail"].ToString();
+            tableCell.Width = Cell2Width;
+            tableRow.Cells.Add(tableCell);
+            Table1.Rows.Add(tableRow);
+
+            tableRow = new TableRow();
+            tableCell = new TableCell();
+            tableCell.Text = "予約席及び料金：";
+            tableCell.RowSpan = seatList.Count + 2;
+            tableRow.Cells.Add(tableCell);
+            tableCell = new TableCell();
+            tableCell.Text = "予約席";
+            tableCell.Width = Cell2Width;
+            tableRow.Cells.Add(tableCell);
+            tableCell = new TableCell();
+            tableCell.Text = "チケットの種類";
+            tableRow.Cells.Add(tableCell);
+            tableCell = new TableCell();
+            tableCell.Text = "料金";
+            tableRow.Cells.Add(tableCell);
+            Table1.Rows.Add(tableRow);
+            int totalcost = 0;//チケットの合計金額を格納する
+
+            for (int i = 0; i < seatList.Count; i++)
+            {
+                tableRow = new TableRow();
+                tableCell = new TableCell();
+                tableCell.Text = seatList[i];//予約席名
+                tableCell.Width = Cell2Width;
+                tableRow.Cells.Add(tableCell);
+                tableCell = new TableCell();
+                tableCell.Text = selectedticketList[i].ticketName;//各予約席のチケットの種類
+                tableRow.Cells.Add(tableCell);
+                tableCell = new TableCell();
+                tableCell.Text = selectedticketList[i].ticketRate.ToString();//チケットの種類に基づく料金
+                tableRow.Cells.Add(tableCell);
+                Table1.Rows.Add(tableRow);
+                totalcost += selectedticketList[i].ticketRate;//チケットの料金を足す
+            }
+
+            tableRow = new TableRow();
+            tableCell = new TableCell();
+            tableCell.Text = "合計" + seatList.Count.ToString() + "席";
+            tableCell.Width = Cell2Width;
+            tableRow.Cells.Add(tableCell);
+            tableCell = new TableCell();
+            tableCell.Text = "合計金額";
+            tableRow.Cells.Add(tableCell);
+            tableCell = new TableCell();
+            tableCell.Text = totalcost.ToString();
+            tableRow.Cells.Add(tableCell);
+            Table1.Rows.Add(tableRow);
+
+            tableRow = new TableRow();
+            tableCell = new TableCell();
+            tableCell.Text = "以上の内容でよろしいでしょうか";
+            tableCell.ColumnSpan = 2;
+            tableRow.Cells.Add(tableCell);
+            Table1.Rows.Add(tableRow);
+
+            tableRow = new TableRow();
+            tableCell = new TableCell();
+            Button BackBtn = new Button();
+            BackBtn.Text = "戻る";
+            BackBtn.Click += new System.EventHandler(this.BackBtn_Click);
+            tableCell.Controls.Add(BackBtn);
+            tableRow.Cells.Add(tableCell);
+            tableCell = new TableCell();
+            Button AcceptBtn = new Button();
+            AcceptBtn.Text = "確定";
+            AcceptBtn.Click += new System.EventHandler(this.AcceptBtn_Click);
+            tableCell.Controls.Add(AcceptBtn);
+            tableRow.Cells.Add(tableCell);
+            Table1.Rows.Add(tableRow);
+        }
+
+        protected void BackBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Reservation_Ticket_Selection.aspx");
+        }
+
+        protected void AcceptBtn_Click(object sender, EventArgs e)
+        {
+            seatList = (List<string>)Session["SeatInformation"];//Sessionに格納されてる選択された座席情報を使えるようにseatListに展開する
+            selectedticketList = (List<SelectedTicket>)Session["SelectedTicket"];//引き継いだ選択されたチケット情報を格納する
+            //仮予約テーブルの該当する席を削除する
+            OleDbConnection cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=|DataDirectory|BookingDB.accdb;");
+            OleDbDataAdapter daTemporary = new OleDbDataAdapter("DELETE FROM TBL_TEMPORARY WHERE TEMPORARY_SCHEDULE='" + (string)Session["ScheduleID"] + "'" + SQLWhereMaker.SQLMakeAND(seatList, "TEMPORARY_SEAT"), cn);
+            DataTable dtTemporary = new DataTable();
+            daTemporary.Fill(dtTemporary);
+            //ログイン中の時に会員テーブルのポイントの更新
+            if ((string)Session["MemberID"] != "")
+            {
+                int SUMPoint = 0;
+                for (int i = 0; i < selectedticketList.Count; i++)
+                {
+                    if (selectedticketList[i].ticketName == "6ポイントで一回無料")
+                        SUMPoint -= 6;
+                    else
+                        SUMPoint += 1;
+                }
+                OleDbDataAdapter daMember = new OleDbDataAdapter("UPDATE TBL_MEMBER SET MEMBER_POINT = MEMBER_POINT +" + SUMPoint + "  WHERE MEMBER_ID='" + (string)Session["MemberID"] + "'", cn);
+                DataTable dtMember = new DataTable();
+                daMember.Fill(dtMember);
+            }
+            //予約テーブルに予約情報と売上を追加する
+            //BOOKING_ID = MAX(BOOKING_ID) + 1
+            //OleDbDataAdapter daBooking = new OleDbDataAdapter("INSERT INTO TBL_BOOKING VALUES ('CONVERT(int, '1234')BOOKING_ID = MAX(BOOKING_ID) + 1')", cn);
+
+            Response.Redirect("Reservation_Receipt_Confirmation.aspx");
+        }
+    }
+}
